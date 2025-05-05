3de4a214ed08c6af17a84ef76edbd63442227b98
Hameez Khan <44177405+HameezKhan85@users.noreply.github.com>
2025-05-05 23:29:23 +0500

    using useSWR hook make api data rendered on page without reloading.

--- app/page.js
+++ app/page.js
@@ -1,17 +1,24 @@
-import RecentPosts from '@/components/RecentPosts'
-import AllPosts from '@/components/AllPosts'
+"use client";
 
-async function getPosts() {
-  const response = await fetch('http://localhost:3000/data/posts.json');
-  return await response.json();
-}
+import RecentPosts from '@/components/RecentPosts';
+import AllPosts from '@/components/AllPosts';
+import useSWR from 'swr';
 
-export default async function Home() {
-  const allPosts = await getPosts();
-  const sortedRecentPosts = [...allPosts].sort((a, b) =>
-    new Date(b.date) - new Date(a.date)
-  ).slice(0, 4);
-  
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function Home() {
+  const { data, error, isLoading } = useSWR('http://localhost:3000/data/posts.json', fetcher, {
+    refreshInterval: 5000,
+    revalidateOnFocus: true, 
+    revalidateOnReconnect: true,
+  });
+
+  if (isLoading) return console.log('Loading...');
+  if (error) return console.log('Error fetching data:', error);
+
+  const sortedPosts = [...data].sort((a, b) => new Date(b.date) - new Date(a.date));
+  const recentPosts = sortedPosts.slice(0, 4);
+
   return (
     <>
       <div className='mb-5 lg:mb-7.5'>
@@ -21,7 +28,7 @@ export default async function Home() {
           </div>
         </div>
       </div>
-      <RecentPosts posts={sortedRecentPosts} />
+      <RecentPosts posts={recentPosts} />
       {/* <AllPosts posts={allPosts} /> */}
     </>
   )
